8e7b1e1023f8700c9866749befce5744832ed496
tobeqz <vanveenjorik@gmail.com>
2022-04-04 10:04:30 +0200

    fix connect

--- placenlbot.user.js
+++ placenlbot.user.js
@@ -7,7 +7,7 @@
 // @match        https://www.reddit.com/r/place/*
 // @match        https://new.reddit.com/r/place/*
 // @connect      reddit.com
-// @connect      placenl.noahvdaa.me
+// @connect      place.tobeqz-on.top
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @require	     https://cdn.jsdelivr.net/npm/toastify-js
 // @resource     TOASTIFY_CSS https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css
